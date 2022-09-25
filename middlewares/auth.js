bbb988bfc22a1eae8eec3e5ce8484f5e72faa2c2
SkylynxX <meshcheriakova.elizaveta@gmail.com>
2022-09-25 22:44:34 +0300

    git auth token secure

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,14 @@ const { JWT_SECRET = 'dev-secret' } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
+  if (!token) {
+    throw new ErrorNotAuthorized();
+  }
   let payload;
-  if (token) {
-    try {
-      payload = jwt.verify(token, JWT_SECRET);
-    } catch (err) {
-      next(new ErrorNotAuthorized());
-    }
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    next(new ErrorNotAuthorized());
   }
   req.user = payload;
   return next();

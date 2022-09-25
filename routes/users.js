57ac1f426b8b983bff3385bfd64675f99b928ff3
SkylynxX <meshcheriakova.elizaveta@gmail.com>
2022-09-25 23:50:55 +0300

    fix order for user me before user id

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,8 @@ const {
 } = require('../middlewares/validation');
 
 routerUsers.get('/users', getUsers);
-routerUsers.get('/users/:userId', validateUserID, getUserByID);
 routerUsers.get('/users/me', getUser);
+routerUsers.get('/users/:userId', validateUserID, getUserByID);
 routerUsers.post('/users', validateUser, createUser);
 routerUsers.patch('/users/me', validateUserInfo, updateUserInfoByID);
 routerUsers.patch('/users/me/avatar', validateAvatar, updateUserAvatarByID);
